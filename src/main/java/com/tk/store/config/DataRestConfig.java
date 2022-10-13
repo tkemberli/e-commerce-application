6d5dc32d1b254d2419f6b047eea4ec9fc1d6bf2f
tkemberli@gmail.com <tkemberli@gmail.com>
2022-10-13 11:40:04 -0300

    Exposes the Ids for the products in the Json responses

--- src/main/java/com/tk/store/config/DataRestConfig.java
+++ src/main/java/com/tk/store/config/DataRestConfig.java
@@ -2,16 +2,29 @@ package com.tk.store.config;
 
 import com.tk.store.entity.Product;
 import com.tk.store.entity.ProductCategory;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-@Configuration
-public class DataRestConfig implements RepositoryRestConfigurer {
-    @Override
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+@Configuration
+@RequiredArgsConstructor
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private final EntityManager entityManager;
+    
+    @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
@@ -30,5 +43,20 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                         httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) ->
                         httpMethods.disable(unsupportedActions));
+        
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        val entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for(EntityType<?> entityType: entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
